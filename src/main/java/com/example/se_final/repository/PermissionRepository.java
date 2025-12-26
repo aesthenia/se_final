d1d3f9dca22f68f6a9d9f22d77498748d2b3778d
aesthenia <aesthenia2@gmail.com>
2025-12-26 15:30:19 +0500

    application.properties was changed for db and permission entity and repository is created

---
+++ src/main/java/com/example/se_final/repository/PermissionRepository.java
@@ -0,0 +1,10 @@
+package com.example.se_final.repository;
+
+import com.example.se_final.model.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Permission findByName(String name);
+}
